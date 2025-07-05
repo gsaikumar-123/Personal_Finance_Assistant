f23b8d63bd3784ac37ed080c3906a79faf10ea6c
G SAI KUMAR <gsaikumar2505@gmail.com>
2025-07-05 11:09:28 +0530

    Added transaction model

---
+++ backend/src/models/transaction.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+    userId : {
+        type : mongoose.Schema.Types.objectId,
+        ref: 'User',
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['income', 'expense'],
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        enum: ['salary','business','investments','other','food','rent','transport','entertainment','utilities','healthcare','education','other'],
+        default: 'other'
+    },
+    date : {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    description : String,
+    paymentMethod : {
+        type: String,
+        enum: ['cash','creditcard','debitcard','banktransfer','other'],
+        default: 'other'
+    }
+});
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+module.exports = Transaction;
